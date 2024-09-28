f59bdeb4a099896aca94ca06f92462fe88616b08
Vanilson Sousa <vanilsonped7@gmail.com>
2024-09-28 11:09:55 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Operacoes.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio2;
+public class Operacoes
+{
+    static public void Adicao(double numero1, double numero2)
+    {
+        var resultado = numero1 + numero2;
+        Console.WriteLine(resultado);
+    }
+
+    static public void Subtracao(double numero1, double numero2)
+    {
+        var resultado = numero1 - numero2;
+        Console.WriteLine(resultado);
+    }
+
+    static public void Multiplicacao(double numero1, double numero2)
+    {
+        var resultado = numero1 * numero2;
+        Console.WriteLine(resultado);
+    }
+
+    static public void Divisao(double numero1, double numero2) {
+        if (numero2 == 0) {
+            Console.WriteLine("Não é possível divisão por zero!");
+            return;
+        }
+        var resultado = numero1 / numero2;
+        Console.WriteLine(resultado);
+    }
+
+    static public void Media(double numero1, double numero2) { 
+        var media = (numero1 + numero2) / 2;
+        Console.WriteLine(media);
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio2;
+
+public class Program
+{
+    public static void Main()
+    {
+        Operacoes.Adicao(numero1: 11, numero2: 17);
+        Operacoes.Subtracao(numero1: 20, numero2: 17);
+        Operacoes.Multiplicacao(numero1: 2, numero2: 7);
+        Operacoes.Divisao(numero1: 10, numero2: 2);
+        Operacoes.Media(numero1: 10, numero2: 2);
+    }
+}
